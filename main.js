03b87d78b0cfa7bbf59b0f831fc37b145363c545
Christian Hombach <christian@homba.ch>
2020-12-08 23:38:11 +0100

    0.3.3

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ class go_e_charger extends utils.Adapter {
                 */
             this.log.debug(`OnReady done`);
             await this.Read_Charger();
-            this.log.debug(`Initial ReadCharger done, detected firmware ${Firmware} ` & Firmware);
+            this.log.debug(`Initial ReadCharger done, detected firmware ${Firmware} ` + Firmware);
             switch (Firmware) {
                 case '033' || '040':
                     this.log.debug(`Init done, launching state machine`);
