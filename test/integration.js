a04ac4aead3086b2b3e92d9b78ebe8e826421a9d
hombach <63002482+hombach@users.noreply.github.com>
2022-07-31 22:39:25 +0200

    0.15.0

--- test/integration.js
+++ test/integration.js
@@ -1,6 +1,6 @@
 //const path = require('path');
 //const { tests } = require('@iobroker/testing');
-const path = import 'path';
+const path = import './path';
 const { tests } = import '@iobroker/testing';
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
