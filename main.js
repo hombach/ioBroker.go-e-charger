66f74dadb6a74554bcf16a4954668d0606695a34
hombach <63002482+hombach@users.noreply.github.com>
2022-08-10 21:47:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // The adapter-core module gives you access to the core ioBroker functions, you need to create an adapter
 // const utils = require('@iobroker/adapter-core');
-const utils = import '@iobroker/adapter-core'
+import utils from '@iobroker/adapter-core'
 //Replace all require()/module.export with import/export
 
 // Load your modules here, e.g.:
